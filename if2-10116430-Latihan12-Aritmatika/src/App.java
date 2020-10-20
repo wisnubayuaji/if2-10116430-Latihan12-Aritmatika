5e63f4b5a931683741c56ff41033bcd5f51c020b
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-20 19:45:05 +0700

    Add files via upload

---
+++ if2-10116430-Latihan12-Aritmatika/src/App.java
@@ -0,0 +1,24 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (b / a));
+        System.out.println("a % b = " + (b % a));
+        System.out.println("a % b = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+}
